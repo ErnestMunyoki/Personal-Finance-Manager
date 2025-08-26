8f5393e24888053f34787b222c47dfd2f2a9e488
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-08-26 20:18:37 +0300

    insertions made to manager.py

---
+++ finance/manager.py
@@ -0,0 +1,22 @@
+class FinanceManager:
+    def __init__(self):
+        self.transaction = []
+        price("Finance Manager initialized.")
+
+    def add_transaction(self):
+        print("[TODO] Add a new transaction.")
+
+    def view_transaction(self):
+        print("[TODO] Display all transactions.")
+
+    def generate_report(self):
+        print("[TODO] Generate reports and summaries.")
+
+    def manage_budget(self):
+        print("[TODO] Mangae budget and show alerts.")
+
+    def search_transactions(self):
+        print("[TODO] Search and filter transactions.")
+
+    def backup_restore(self):
+        print("[TODO] Backup and restore data.")
