f52b91974cd8f3fe3efa177285d90429420c48c1
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-08-27 20:02:08 +0300

    changes made to transaction.py

--- finance/transaction.py
+++ finance/transaction.py
@@ -1,13 +1,29 @@
-class Transaction: 
+class Transaction:
     def __init__(self, amount, category, date, description, t_type="expense"):
-        """
-        t_type: "income" or "expense"
-        """
         self.amount = amount
         self.category = category
         self.date = date
         self.description = description
-        self.type = t_type
+        self.type = t_type 
 
-    def __str__(self):
-        return f"{self.date} | {self.type.upper()} | {self.category} | {self.amount} | {self.description}"
+    def to_dict(self):
+        """Convert transaction object into dictionary for saving."""
+        return {
+            "amount": self.amount,
+            "category": self.category,
+            "date": self.date,
+            "description": self.description,
+            "type": self.type
+        }
+
+    @staticmethod
+    def from_dict(data):
+        """Create Transaction object from dictionary."""
+        return Transaction(
+            amount=data.get("amount"),
+            category=data.get("category"),
+            date=data.get("date"),
+            description=data.get("description"),
+            t_type=data.get("type", "expense")
+        )
+
